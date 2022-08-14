180187bbd5749e511a895cc1cd08a3f9a1c678c4
Dinhminhduc <72441314+Dinhminhduc@users.noreply.github.com>
2022-08-14 20:41:06 +0700

    Update IntroScreen.jsx

--- StudyBeatsFM/src/components/IntroScreen.jsx
+++ StudyBeatsFM/src/components/IntroScreen.jsx
@@ -24,8 +24,8 @@ const Hero = () => {
         </div>
         <div className="titleContainer">
           <img className="heroLogo" src={logo} alt="" />
-          <h1 className="tracking-in-expand-fwd">Hoang Lan</h1>
-          <h3 className="subTitle"> Forgive me :3 </h3>
+          <h1 className="tracking-in-expand-fwd">For Nhung</h1>
+          <h3 className="subTitle"> Nhung's Music </h3>
           <div class="note-position-1 note-amination">&#9835;</div>
           <div class="note-position-2 note-amination animation-delay-2">
             &#9833;
